1dca898d2faa1ba7fb761e81310cc1040fdc1543
Hossain Ahmad <haj.ctg@gmail.com>
2026-01-11 09:50:05 +0000

    responsive menu update second

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -73,14 +73,14 @@ export default function Header() {
         <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Wisdom</span>
               <img
                 alt=""
                 src="./image/logo/logo.svg"
                 className="h-8 w-auto"
               />
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -93,38 +93,38 @@ export default function Header() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-white/10 font-serif">
               <div className="space-y-2 py-6">
-                <a
+                <Link
                   href="/#book-three"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-normal text-slate-500 hover:bg-white/5"
                 >
                   Books
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/#articles"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-normal text-slate-500 hover:bg-white/5"
                 >
                   Articles
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/#news"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-normal text-slate-500 hover:bg-white/5"
                 >
                   News
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/#contact"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-normal text-slate-500 hover:bg-white/5"
                 >
                   Contact
-                </a>
+                </Link>
               </div>
               <div className="py-6">
-                <a
+                <Link
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-normal text-slate-500 hover:bg-white/5"
                 >
                   Log in
-                </a>
+                </Link>
               </div>
             </div>
           </div>
